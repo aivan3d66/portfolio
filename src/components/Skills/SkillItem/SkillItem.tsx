0d1ab816ea2fc67468746e4487f59c275d5d01ee
Ivan <adamovskij00@mail.ru>
2022-01-31 22:31:46 +0300

    add basic SkillItem markup

---
+++ src/components/Skills/SkillItem/SkillItem.tsx
@@ -0,0 +1,17 @@
+import style from "./SkillItem.module.css";
+import React from "react";
+
+type SkillItemType = {
+  title: string,
+  text: string
+}
+
+export const SkillItem: React.FC<SkillItemType> = ({title, text}) => {
+  return (
+    <div className={style.skillItem}>
+      <div className={style.skillItem__img}/>
+      <div className={style.skillItem__title}>{title}</div>
+      <div className={style.skillItem__text}>{text}</div>
+    </div>
+  )
+}

4005b4b54dc3af254e78e5eeca5cbd7cffe4adf3
Ivan <adamovskij00@mail.ru>
2022-01-31 22:30:47 +0300

    add import of Header, AboutMe, Skills, Works components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header/Header";
+import {AboutMe} from "./components/AboutMe/AboutMe";
+import {Skills} from "./components/Skills/Skills";
+import {Works} from "./components/Works/Works";
 
 function App() {
   return (
     <div className="App">
-      Portfolio
+      <Header/>
+      <AboutMe/>
+      <Skills/>
+      <Works/>
     </div>
   );
 }

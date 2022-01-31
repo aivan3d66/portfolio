c1f5f6612fb7ef0e45d26ba7a77a9c13bb4ca25a
Ivan <adamovskij00@mail.ru>
2022-01-31 22:31:40 +0300

    add basic Navigation markup

---
+++ src/components/Header/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import style from './Navigation.module.css';
+
+export const Navigation = () => {
+  return (
+    <ul className={style.navList}>
+      <li className={style.navList__item}>Main</li>
+      <li className={style.navList__item}>Skills</li>
+      <li className={style.navList__item}>Works</li>
+      <li className={style.navList__item}>Contacts</li>
+    </ul>
+  )
+}

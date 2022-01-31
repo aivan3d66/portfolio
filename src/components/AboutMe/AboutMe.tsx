d666f3fa5f7a1a37cfa50e2cc3425403ea583661
Ivan <adamovskij00@mail.ru>
2022-01-31 22:32:17 +0300

    add basic AboutMe markup

---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,18 @@
+import style from "./AboutMe.module.css";
+
+export const AboutMe = () => {
+  return (
+    <section className={style.aboutContainer}>
+      <div className={style.aboutWrapper}>
+        <div className={style.aboutText}>
+          <span>Hello!</span>
+          <span>My name is Ivan Adamouski</span>
+          <span>I am a Front-end developer</span>
+        </div>
+        <div className={style.aboutImage}>
+          *beautiful me*
+        </div>
+      </div>
+    </section>
+  )
+}

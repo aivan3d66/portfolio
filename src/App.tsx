7743651bd256732343350c00dd90966fef15fa50
Ivan <adamovskij00@mail.ru>
2022-02-01 22:15:28 +0300

    add Footer import

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {Skills} from "./components/Skills/Skills";
 import {Works} from "./components/Works/Works";
 import {Employ} from "./components/Employ/Employ";
 import {Contacts} from "./components/Contacts/Contacts";
+import {Footer} from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Works/>
       <Employ/>
       <Contacts/>
+      <Footer/>
     </div>
   );
 }

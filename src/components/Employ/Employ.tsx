47b7b851baec1fcf7a7b3163a056096522229116
Ivan <adamovskij00@mail.ru>
2022-02-01 21:37:58 +0300

    add basic Employ markup styles

---
+++ src/components/Employ/Employ.tsx
@@ -0,0 +1,12 @@
+import style from './Employ.module.css';
+
+export const Employ = () => {
+  return (
+    <section className={style.employContainer}>
+      <h2>Looking for remote work</h2>
+      <div className={style.employWrapper}>
+        <button>Hire me</button>
+      </div>
+    </section>
+  )
+}

3a9f3b8a49bf5d9fafc65742124536048a51e5bf
Ivan <adamovskij00@mail.ru>
2022-02-01 21:55:34 +0300

    add Contacts import

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {AboutMe} from "./components/AboutMe/AboutMe";
 import {Skills} from "./components/Skills/Skills";
 import {Works} from "./components/Works/Works";
 import {Employ} from "./components/Employ/Employ";
+import {Contacts} from "./components/Contacts/Contacts";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skills/>
       <Works/>
       <Employ/>
+      <Contacts/>
     </div>
   );
 }

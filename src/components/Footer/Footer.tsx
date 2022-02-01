dc3bb3d2fd6f820fe20408bcb48d4cbd34c653c5
Ivan <adamovskij00@mail.ru>
2022-02-01 22:15:01 +0300

    add basic Footer markup

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,15 @@
 import style from './Footer.module.css';
+import { Socials } from './Socials/Socials';
 
 export const Footer = () => {
   return (
-    <section className={style.headerContainer}>
-      <div className={style.headerWrapper}>
-        <Navigation/>
+    <section className={style.footerContainer}>
+      <h2>Ivan Adamouski</h2>
+      <div className={style.footerWrapper}>
+        <Socials/>
+        <div className={style.footerInform}>
+          2022 All rights reserved
+        </div>
       </div>
     </section>
   )

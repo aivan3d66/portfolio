748ab6277bf11a8f8a3784ef4c68e20972f9bbea
Ivan <adamovskij00@mail.ru>
2022-03-04 21:52:27 +0300

    add fix App scss import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import {Header} from "./components/Header/Header";
 import {AboutMe} from "./components/AboutMe/AboutMe";
 import {Skills} from "./components/Skills/Skills";

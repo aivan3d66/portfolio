802edb3f85bd5bee66b55d558bc6659f5f0c3f51
Ivan <adamovskij00@mail.ru>
2022-03-05 22:27:38 +0300

    add upd projects

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -2,30 +2,24 @@ import style from "./Works.module.scss";
 import {WorkItem} from "./WorkItem/WorkItem";
 import {BlockTitle} from "../../common/BlockTitle";
 
-type WorkItemType = {
-  id: number,
-  title: string,
-  text: string,
-}
-type WorksType = Array<WorkItemType>
-
 export const Works = () => {
-  const works: WorksType = [
-    {id: 1, title: "Name of project", text: "project description"},
-    {id: 2, title: "Name of project", text: "project description"},
-    {id: 3, title: "Name of project", text: "project description"},
-  ];
-
-  const worksList = works.map(w => <WorkItem title={w.title}
-                                             key={w.id}
-                                             text={w.text}/>)
-
   return (
     <section className={style.worksContainer}>
       <BlockTitle title={"My works"}/>
       <div className={style.worksWrapper}>
         <div className={style.worksList}>
-          {worksList}
+          <WorkItem
+            title={"Todo List"}
+            description={"Simple todo list with storage of data on remote server. Technology stack: React, Redux, REST API, Storybook, Unit and snapshot tests"}
+          />
+          <WorkItem
+            title={"Social Network"}
+            description={"Mini-version of Social network. It is possible to write messages to another users, add posts, and search for new friends."}
+          />
+          <WorkItem
+            title={"Big trip"}
+            description={"Features: The MVP architectural pattern is used, based on the Object-oriented programming (classes)"}
+          />
         </div>
       </div>
     </section>

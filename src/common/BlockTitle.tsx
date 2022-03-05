fcb2ae265fb4dbc0a4d6b6b1d179af855666c03c
Ivan <adamovskij00@mail.ru>
2022-03-05 21:57:21 +0300

    add BlockTitle component

---
+++ src/common/BlockTitle.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from "./BlockTitle.module.scss";
+
+type BlockTitleType = {
+  title: string
+}
+
+export const BlockTitle: React.FC<BlockTitleType> = ({title}) => {
+  return (
+    <div className={s.titleWrapper}>
+      <h2 className={s.titleText}>My skills</h2>
+      <span className={s.titleLine}></span>
+    </div>
+  )
+}

0bac080973110fe146d566276a35043e7515f69f
Ivan <adamovskij00@mail.ru>
2022-03-06 21:39:08 +0300

    add br tag to AboutMe

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -6,7 +6,7 @@ export const AboutMe = () => {
       <div className={style.aboutWrapper}>
         <div className={style.aboutText}>
           <span className={style.aboutText__greeting}>Hello there!</span>
-          <span className={style.aboutText__name}>My name is Ivan Adamouski</span>
+          <span className={style.aboutText__name}>My name <br/>is Ivan Adamouski</span>
           <span className={style.aboutText__profession}>And I am a Front-end Developer</span>
         </div>
         <div className={style.aboutImage}>

12005abc9ec7bb9e667ec242c682292c5727fa03
Ivan <adamovskij00@mail.ru>
2022-03-06 23:21:54 +0300

    add bg-img styles type

--- src/components/Works/WorkItem/WorkItem.tsx
+++ src/components/Works/WorkItem/WorkItem.tsx
@@ -1,20 +1,21 @@
-import style from "./WorkItem.module.scss";
-import React from "react";
+import s from "./WorkItem.module.scss";
+import React, {CSSProperties} from "react";
 
 type WorkItemType = {
   title: string,
-  description: string
+  description: string,
+  style: CSSProperties | undefined
 }
 
-export const WorkItem: React.FC<WorkItemType> = ({title, description}) => {
+export const WorkItem: React.FC<WorkItemType> = ({title, description, style}) => {
   return (
-    <div className={style.workItem}>
-      <div className={style.workPreview}>
-        <a className={style.workPreview__link} href={'#'}>Look</a>
+    <div className={s.workItem}>
+      <div className={s.workPreview} style={style}>
+        <a className={s.workPreview__link} href={'#'}>Look</a>
       </div>
-      <div className={style.workDescription}>
-        <h3 className={style.workItem__title}>{title}</h3>
-        <p className={style.workItem__text}>{description}</p>
+      <div className={s.workDescription}>
+        <h3 className={s.workItem__title}>{title}</h3>
+        <p className={s.workItem__text}>{description}</p>
       </div>
     </div>
   )

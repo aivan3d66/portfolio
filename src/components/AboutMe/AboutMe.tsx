20465db04927c2a386ee2539cab07f0a5f398a49
Ivan <adamovskij00@mail.ru>
2022-03-07 19:04:44 +0300

    add vantaEffect

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,9 +1,30 @@
+import React, {useState, useEffect, useRef} from 'react';
+// @ts-ignore
+import NET from 'vanta/dist/vanta.net.min';
 import style from "./AboutMe.module.scss";
 import photo1 from "./../../assets/images/photo1.jpg";
 
 export const AboutMe = () => {
+  const [vantaEffect, setVantaEffect] = useState<any>(0);
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(NET({
+        el: myRef.current,
+        color: 0x343a40,
+        backgroundColor: 0x1f1f20,
+        maxDistance: 27.00,
+        spacing: 19.00
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
+
   return (
-    <section className={style.aboutContainer}>
+    <section id="my-background" className={style.aboutContainer} ref={myRef}>
       <div className={style.aboutWrapper}>
         <div className={style.aboutText}>
           <span className={style.aboutText__greeting}>Hello there!</span>

607b21241296ce561f0a5841ef5685caa4efcaf8
Ivan <adamovskij00@mail.ru>
2022-03-07 17:08:03 +0300

    add upd Contacts inputs placeholders

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -9,9 +9,9 @@ export const Contacts = () => {
       <div className={style.contactsWrapper}>
         <form className={style.contactsForm} action="#">
           <div className={style.contactsForm__fields}>
-            <input type="text"/>
-            <input type="text"/>
-            <textarea/>
+            <input type="text" placeholder={"Name"}/>
+            <input type="text" placeholder={"E-mail"}/>
+            <textarea placeholder={"Your message"}/>
           </div>
           <button type="submit" className={style.contactsForm__btn}>Send</button>
         </form>

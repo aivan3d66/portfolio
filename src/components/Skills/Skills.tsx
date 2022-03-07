d3727de4ff8def8c4742eb616a73e1568cd5900f
Ivan <adamovskij00@mail.ru>
2022-03-07 11:13:59 +0300

    add Skills refactoring

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -7,28 +7,38 @@ export const Skills = () => {
     <section className={style.skillsContainer}>
       <BlockTitle title={"My skills"}/>
       <div className={style.skillsWrapper}>
-        <div className={style.skillsList}>
-          <SkillItem
-            title={"JavaScript"}
-            text={"Every programmer has once killed entire hours to a funny little problem - two parameters, not in the order, variable that's somewhere with a big letter, but somewhere else not (Thank you JavaScript!"}
-          />
-          <SkillItem
-            title={"TypeScript"}
-            text={"This thing helps to love JavaScript"}
-          />
-          <SkillItem
-            title={"React"}
-            text={"Library? Software framework? We can argue on this subject for a very long time, and it doesn 't make any sense"}
-          />
-          <SkillItem
-            title={"Redux"}
-            text={"A powerful library of state management for the proper construction of an application architecture"}
-          />
-          <SkillItem
-            title={"Unit test / Jest"}
-            text={"TDD -  correct the error before the millions of your nerve cells die"}
-          />
-        </div>
+        <ul className={style.skillsList}>
+          <li>
+            <SkillItem
+              title={"JavaScript"}
+              text={"Every programmer has once killed entire hours to a funny little problem - two parameters, not in the order, variable that's somewhere with a big letter, but somewhere else not (Thank you JavaScript!"}
+            />
+          </li>
+          <li>
+            <SkillItem
+              title={"TypeScript"}
+              text={"This thing helps to love JavaScript"}
+            />
+          </li>
+          <li>
+            <SkillItem
+              title={"React"}
+              text={"Library? Software framework? We can argue on this subject for a very long time, and it doesn 't make any sense"}
+            />
+          </li>
+          <li>
+            <SkillItem
+              title={"Redux"}
+              text={"A powerful library of state management for the proper construction of an application architecture"}
+            />
+          </li>
+          <li>
+            <SkillItem
+              title={"Unit test / Jest"}
+              text={"TDD -  correct the error before the millions of your nerve cells die"}
+            />
+          </li>
+        </ul>
       </div>
     </section>
   )

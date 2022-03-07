d65723800f1b2cd84f2a63e515193a0a2a5d9105
Ivan <adamovskij00@mail.ru>
2022-03-07 11:13:40 +0300

    delete image from SkillItem

--- src/components/Skills/SkillItem/SkillItem.tsx
+++ src/components/Skills/SkillItem/SkillItem.tsx
@@ -1,17 +1,14 @@
 import style from "./SkillItem.module.scss";
 import React from "react";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faReact} from '@fortawesome/free-brands-svg-icons';
 
 type SkillItemType = {
   title: string,
-  text: string
+  text: string,
 }
 
 export const SkillItem: React.FC<SkillItemType> = ({title, text}) => {
   return (
     <div className={style.skillItem}>
-      <FontAwesomeIcon icon={faReact} className={style.skillItem__img}/>
       <h3 className={style.skillItem__title}>{title}</h3>
       <p className={style.skillItem__text}>{text}</p>
     </div>

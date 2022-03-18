d4785515548ebc726ab0e598f0addf820e99685f
Ivan <adamovskij00@mail.ru>
2022-03-18 21:52:09 +0300

    add refactoring App components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.scss';
-import {Header} from "./components/Header/Header";
-import {AboutMe} from "./components/AboutMe/AboutMe";
-import {Skills} from "./components/Skills/Skills";
-import {Works} from "./components/Works/Works";
-import {Employ} from "./components/Employ/Employ";
-import {Contacts} from "./components/Contacts/Contacts";
 import {Footer} from "./components/Footer/Footer";
+import {HeaderSection} from "./common/Scroll";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <AboutMe/>
-      <Skills/>
-      <Works/>
-      <Employ/>
-      <Contacts/>
+      <HeaderSection/>
       <Footer/>
     </div>
   );

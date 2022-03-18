8cdc6aaafa4619b74b22078410191a38630c9ee3
Ivan <adamovskij00@mail.ru>
2022-03-18 22:17:36 +0300

    add change headerContainer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Link, Element, Events, animateScroll as scroll, scroller} from 'react-scroll';
-import style from './Header.module.scss';
 import {AboutMe} from "../AboutMe/AboutMe";
 import {Skills} from "../Skills/Skills";
 import {Works} from "../Works/Works";
 import {Contacts} from "../Contacts/Contacts";
 import {Employ} from "../Employ/Employ";
 import {FaChevronUp} from "react-icons/fa";
+import style from './Header.module.scss';
 
 export class HeaderSection extends React.Component {
 
@@ -72,26 +72,28 @@ export class HeaderSection extends React.Component {
 
   render() {
     return (
-      <section className={style.headerContainer}>
-        <div className={style.headerWrapper} id="bs-example-navbar-collapse-1">
-          <ul className={style.navList}>
-            <li>
-              <Link activeClass="active" className="test1" to="test1" spy={true} smooth={true}
-                    duration={500}>Main</Link>
-            </li>
-            <li>
-              <Link activeClass="active" className="test2" to="test2" spy={true} smooth={true}
-                    duration={500}>Skills</Link>
-            </li>
-            <li>
-              <Link activeClass="active" className="test3" to="test3" spy={true} smooth={true}
-                    duration={500}>Works</Link>
-            </li>
-            <li>
-              <Link activeClass="active" className="test4" to="test4" spy={true} smooth={true}
-                    duration={500}>Contacts</Link>
-            </li>
-          </ul>
+      <section>
+        <div className={style.headerContainer}>
+          <div className={style.headerWrapper}>
+            <ul className={style.navList}>
+              <li>
+                <Link activeClass="active" className="test1" to="test1" spy={true} smooth={true}
+                      duration={500}>Main</Link>
+              </li>
+              <li>
+                <Link activeClass="active" className="test2" to="test2" spy={true} smooth={true}
+                      duration={500}>Skills</Link>
+              </li>
+              <li>
+                <Link activeClass="active" className="test3" to="test3" spy={true} smooth={true}
+                      duration={500}>Works</Link>
+              </li>
+              <li>
+                <Link activeClass="active" className="test4" to="test4" spy={true} smooth={true}
+                      duration={500}>Contacts</Link>
+              </li>
+            </ul>
+          </div>
         </div>
 
         <Element name="test1" className="element">

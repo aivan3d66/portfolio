86ab2ec4891fb7e74d78ec0e6b137311ed06acd4
Ivan <adamovskij00@mail.ru>
2022-03-20 21:24:24 +0300

    add pagesLink to props

--- src/components/Works/WorkItem/WorkItem.tsx
+++ src/components/Works/WorkItem/WorkItem.tsx
@@ -4,14 +4,15 @@ import React, {CSSProperties} from "react";
 type WorkItemType = {
   title: string,
   description: string,
+  pagesLink: string
   style: CSSProperties | undefined
 }
 
-export const WorkItem: React.FC<WorkItemType> = ({title, description, style}) => {
+export const WorkItem: React.FC<WorkItemType> = ({title, pagesLink, description, style}) => {
   return (
     <div className={s.workItem}>
       <div className={s.workPreview} style={style}>
-        <a className={s.workPreview__link} href={'#'}>Look</a>
+        <a className={s.workPreview__link} href={pagesLink}>Look</a>
       </div>
       <div className={s.workDescription}>
         <h3 className={s.workItem__title}>{title}</h3>

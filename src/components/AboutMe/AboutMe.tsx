c6b46975b88d5d8e5dbf740f8267ffb4dcade4d3
Ivan <adamovskij00@mail.ru>
2022-03-22 22:20:10 +0300

    add AboutMe animation

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -3,7 +3,32 @@ import React, {useState, useEffect, useRef} from 'react';
 import NET from 'vanta/dist/vanta.net.min';
 import style from "./AboutMe.module.scss";
 import photo1 from "./../../assets/images/photo1.jpg";
+import {motion} from 'framer-motion';
 
+const list = {
+  hidden: {
+    opacity: 1,
+    transition: {when: "afterChildren"}
+  }
+}
+
+const item = {
+  hidden: {
+    opacity: 1,
+    transition: {duration: 2}
+  }
+}
+const container = {
+  hidden: { opacity: 0, x: -1000},
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      delayChildren: 1,
+      delay: 2
+    }
+  }
+}
 export const AboutMe = () => {
   const [vantaEffect, setVantaEffect] = useState<any>(0);
   const myRef = useRef(null);
@@ -24,19 +49,38 @@ export const AboutMe = () => {
   }, [vantaEffect])
 
   return (
-    <section id="my-background" className={style.aboutContainer} ref={myRef}>
+    <motion.section
+      animate="hidden"
+      variants={list}
+      id="my-background"
+      className={style.aboutContainer}
+      ref={myRef}
+    >
       <div className={style.aboutWrapper}>
-        <div className={style.aboutText}>
+        <motion.div
+          variants={item}
+          animate="hidden"
+          className={style.aboutText}
+        >
           <span className={style.aboutText__greeting}>Hello there!</span>
           <span className={style.aboutText__name}>My name is<br/>
             <b>Ivan Adamouski</b>
           </span>
           <span className={style.aboutText__profession}>And I am a Front-end Developer</span>
-        </div>
-        <div className={style.aboutImage}>
-          <img src={photo1} alt="avatar"/>
-        </div>
+        </motion.div>
+        <motion.div
+          className={style.aboutImage}
+          variants={container}
+          initial="hidden"
+          animate="show"
+        >
+          <motion.img
+            src={photo1}
+            alt="avatar"
+
+          />
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   )
 }

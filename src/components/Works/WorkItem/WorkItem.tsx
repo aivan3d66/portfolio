ea1fbe7e9bb1d9798e7c7f5907dcb1c8b113d54d
Ivan <adamovskij00@mail.ru>
2022-03-22 12:01:20 +0300

    add upd WorkItem props type and add githubLink

--- src/components/Works/WorkItem/WorkItem.tsx
+++ src/components/Works/WorkItem/WorkItem.tsx
@@ -4,15 +4,17 @@ import React, {CSSProperties} from "react";
 type WorkItemType = {
   title: string,
   description: string,
-  pagesLink: string
+  pagesLink: string,
+  githubLink: string,
   style: CSSProperties | undefined
 }
 
-export const WorkItem: React.FC<WorkItemType> = ({title, pagesLink, description, style}) => {
+export const WorkItem: React.FC<WorkItemType> = ({title, pagesLink, description, githubLink, style}) => {
   return (
     <div className={s.workItem}>
       <div className={s.workPreview} style={style}>
-        <a className={s.workPreview__link} href={pagesLink}>Open</a>
+        <a className={s.workPreview__link} href={pagesLink}>Demo</a>
+        <a className={s.workPreview__link} href={githubLink}>Code</a>
       </div>
       <div className={s.workDescription}>
         <h3 className={s.workItem__title}>{title}</h3>

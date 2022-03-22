ae33510713299b038cb11a0957adb6e04c8b27bb
Ivan <adamovskij00@mail.ru>
2022-03-22 12:00:53 +0300

    add githubLink to WorkItem prop

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -33,24 +33,28 @@ export const Works = () => {
             style={todoListBgImg}
             title={"Todo List"}
             pagesLink={'https://aivan3d66.github.io/toDoList-TS/'}
+            githubLink={'https://github.com/aivan3d66/toDoList-TS'}
             description={"Simple todo list with storage of data on remote server. Technology stack: React, Redux (Redux-thunk), REST API, Storybook, Unit and snapshot tests"}
           />
           <WorkItem
             style={socialNetworkBgImg}
             title={"Social Network"}
             pagesLink={'https://aivan3d66.github.io/socialNetwork-project/'}
+            githubLink={'https://github.com/aivan3d66/socialNetwork-project'}
             description={"Mini-version of Social network. It is possible to write messages to another users, add posts, and search for new friends."}
           />
           <WorkItem
             style={bigTripBgImg}
             title={"Big trip"}
             pagesLink={'https://aivan3d66.github.io/Big-trip/'}
+            githubLink={'https://github.com/aivan3d66/Big-trip'}
             description={"Features: The MVP architectural pattern is used, based on the Object-oriented programming (classes), Off-line mode (Service Worker)"}
           />
           <WorkItem
             style={maroonBgImg}
             title={"Maroon"}
             pagesLink={'https://aivan3d66.github.io/Maroon/'}
+            githubLink={'https://github.com/aivan3d66/Maroon'}
             description={"E-shop website with main, catalog, product card pages. Adaptive design (desktop, tablet, mobile) flex and grid-based layout, \"Mobile first\""}
           />
         </div>

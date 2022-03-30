5b8c76bbc28fd3c60460cb4bd0fe416286f8a9df
Ivan <adamovskij00@mail.ru>
2022-03-30 10:48:03 +0300

    add new WorkItem

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -2,17 +2,19 @@ import style from "./Works.module.scss";
 import {WorkItem} from "./WorkItem/WorkItem";
 import {BlockTitle} from "../../common/Title/BlockTitle";
 // @ts-ignore
-import todos from "../../assets/images/todos.PNG";
+import todo from "../../assets/images/todo.PNG";
 // @ts-ignore
 import trip from "./../../assets/images/trip.PNG";
 // @ts-ignore
 import social from "./../../assets/images/social.PNG";
 // @ts-ignore
 import maroon from "./../../assets/images/maroonPrewiev.PNG";
+// @ts-ignore
+import converter from "./../../assets/images/converter.PNG";
 
 export const Works = () => {
   const todoListBgImg = {
-    backgroundImage: `url(${todos})`,
+    backgroundImage: `url(${todo})`,
   };
   const socialNetworkBgImg = {
     backgroundImage: `url(${social})`,
@@ -22,6 +24,9 @@ export const Works = () => {
   };
   const maroonBgImg = {
     backgroundImage: `url(${maroon})`,
+  };
+  const converterImg = {
+    backgroundImage: `url(${converter})`,
   };
 
   return (
@@ -37,13 +42,6 @@ export const Works = () => {
             description={"Simple todo list with storage of data on remote server. Technology stack: React, Redux (Redux-thunk), REST API, Storybook, Unit and snapshot tests"}
           />
           <WorkItem
-            style={socialNetworkBgImg}
-            title={"Social Network"}
-            pagesLink={'https://aivan3d66.github.io/socialNetwork-project/'}
-            githubLink={'https://github.com/aivan3d66/socialNetwork-project'}
-            description={"Mini-version of Social network. It is possible to write messages to another users, add posts, and search for new friends."}
-          />
-          <WorkItem
             style={bigTripBgImg}
             title={"Big trip"}
             pagesLink={'https://aivan3d66.github.io/Big-trip/'}
@@ -51,11 +49,25 @@ export const Works = () => {
             description={"Features: The MVP architectural pattern is used, based on the Object-oriented programming (classes), Off-line mode (Service Worker)"}
           />
           <WorkItem
+            style={socialNetworkBgImg}
+            title={"Social Network"}
+            pagesLink={'https://aivan3d66.github.io/socialNetwork-project/'}
+            githubLink={'https://github.com/aivan3d66/socialNetwork-project'}
+            description={"Mini-version of Social network. It is possible to write messages to another users, add posts, and search for new friends."}
+          />
+          <WorkItem
             style={maroonBgImg}
             title={"Maroon"}
             pagesLink={'https://aivan3d66.github.io/Maroon/'}
             githubLink={'https://github.com/aivan3d66/Maroon'}
             description={"E-shop website with main, catalog, product card pages. Adaptive design (desktop, tablet, mobile) flex and grid-based layout, \"Mobile first\""}
+          />
+          <WorkItem
+            style={converterImg}
+            title={"Currency exchange"}
+            pagesLink={'https://currency-exchange-converter.herokuapp.com/'}
+            githubLink={'https://github.com/aivan3d66/currency-converter'}
+            description={"Simple Currency exchange app. Using stack: React + MobX library of state menagment, Styled components and Heroku deploying"}
           />
         </div>
       </div>

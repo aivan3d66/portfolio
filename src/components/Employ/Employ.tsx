d6530ef31a2ce0ca275ce0d2a47b883b167d3d44
Ivan <adamovskij00@mail.ru>
2022-04-08 15:48:04 +0300

    add change of EmployMe component

--- src/components/Employ/Employ.tsx
+++ src/components/Employ/Employ.tsx
@@ -1,14 +1,24 @@
 import style from './Employ.module.scss';
 import {BlockTitle} from "../../common/Title/BlockTitle";
-import {Button} from "../../common/Button/Button";
 
 export const Employ = () => {
-  return (
-    <section className={style.employContainer}>
-      <BlockTitle title={"Looking for remote work"}/>
-      <div className={style.employWrapper}>
-        <Button>Hire me</Button>
-      </div>
-    </section>
-  )
+    return (
+        <section className={style.employContainer}>
+            <BlockTitle title={"A little bit about me"}/>
+            <div className={style.employWrapper}>
+                <p>
+                    Hello there! I'm Front-end developer with strong experience in creating professional Web-interfaces,
+                    Landing pages and SPA. After working as a freelancer, I would like to join some ambitious team
+                    to expand my professional skills. My free time is dedicated to solving katas on Codewars,
+                    improvement my technical skills and boosting my English. I plan to learn powerful framework in the
+                    near future,
+                    Angular or Vue.js I haven 't decided yet. I am always happy to learn something new
+                    (especially, if it is still in front-end branch) :)
+                </p>
+                <p>
+                    P.S. If you read this text, may the IT-Force be with you! ^_^
+                </p>
+            </div>
+        </section>
+    )
 }

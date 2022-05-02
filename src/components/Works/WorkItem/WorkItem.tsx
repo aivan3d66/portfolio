c28f6caf9fff8b7c66e8e75b5085f742828ca8ee
Ivan <adamovskij00@mail.ru>
2022-05-02 22:25:52 +0300

    add workPreview__wrapper

--- src/components/Works/WorkItem/WorkItem.tsx
+++ src/components/Works/WorkItem/WorkItem.tsx
@@ -12,9 +12,11 @@ type WorkItemType = {
 export const WorkItem: React.FC<WorkItemType> = ({title, pagesLink, description, githubLink, style}) => {
   return (
     <div className={s.workItem}>
-      <div className={s.workPreview} style={style} tabIndex={0}>
-        <a className={s.workPreview__link} href={pagesLink} tabIndex={0} target="_blank">Demo</a>
-        <a className={s.workPreview__link} href={githubLink} tabIndex={0} target="_blank">Code</a>
+      <div className={s.workPreview__wrapper}>
+        <div className={s.workPreview} style={style} tabIndex={0}>
+          <a className={s.workPreview__link} href={pagesLink} tabIndex={0} target="_blank">Demo</a>
+          <a className={s.workPreview__link} href={githubLink} tabIndex={0} target="_blank">Code</a>
+        </div>
       </div>
       <div className={s.workDescription}>
         <h3 className={s.workItem__title}>{title}</h3>

94540fab99a16d52b5cc797cae724db61a2a2446
Ivan <adamovskij00@mail.ru>
2022-06-11 18:05:45 +0300

    add WorkItem props type

--- src/components/Works/WorkItem/WorkItem.tsx
+++ src/components/Works/WorkItem/WorkItem.tsx
@@ -1,19 +1,19 @@
 import s from "./WorkItem.module.scss";
-import React, {CSSProperties} from "react";
+import React from "react";
 
 type WorkItemType = {
   title: string,
   description: string,
   pagesLink: string,
   githubLink: string,
-  style: CSSProperties | undefined
+  style: string
 }
 
 export const WorkItem: React.FC<WorkItemType> = ({title, pagesLink, description, githubLink, style}) => {
   return (
     <div className={s.workItem}>
       <div className={s.workPreview__wrapper}>
-        <div className={s.workPreview} style={style} tabIndex={0}>
+        <div className={s.workPreview} style={{backgroundImage: style}} tabIndex={0}>
           <a className={s.workPreview__link} href={pagesLink} tabIndex={0} target="_blank">Demo</a>
           <a className={s.workPreview__link} href={githubLink} tabIndex={0} target="_blank">Code</a>
         </div>

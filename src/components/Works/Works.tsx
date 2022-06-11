940091af8a456ed3bc0c026bbc41cb9117c3e87b
Ivan <adamovskij00@mail.ru>
2022-06-11 18:05:33 +0300

    add Works component refactoring

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,115 +1,28 @@
 import style from "./Works.module.scss";
 import {WorkItem} from "./WorkItem/WorkItem";
 import {BlockTitle} from "../../common/Title/BlockTitle";
-import todo from "../../assets/images/todo-prew.png";
-import trip from "../../assets/images/trip.png";
-import social from "../../assets/images/social-prew.png";
-import maroon from "../../assets/images/maroonPrewiev.png";
-import converter from "../../assets/images/converter.png";
-import game from "../../assets/images/game.png";
-import card from "../../assets/images/card-prew.png";
-import movie from "../../assets/images/movie.png";
-import shop from "./../../assets/images/foodShop-prew.png";
+import {WorksData} from "../../data/mock-data";
 
 export const Works = () => {
-  const todoListBgImg = {
-    backgroundImage: `url(${todo})`,
-  };
-  const socialNetworkBgImg = {
-    backgroundImage: `url(${social})`,
-  };
-  const bigTripBgImg = {
-    backgroundImage: `url(${trip})`,
-  };
-  const maroonBgImg = {
-    backgroundImage: `url(${maroon})`,
-  };
-  const converterImg = {
-    backgroundImage: `url(${converter})`,
-  };
-  const gameImg = {
-    backgroundImage: `url(${game})`,
-  };
-  const cardImg = {
-    backgroundImage: `url(${card})`,
-  };
-  const movieImg = {
-    backgroundImage: `url(${movie})`,
-  };
-  const shopImg = {
-    backgroundImage: `url(${shop})`,
-  };
-
   return (
     <section className={style.worksContainer}>
       <BlockTitle title={"My works"}/>
       <div className={style.worksWrapper}>
         <div className={style.worksList}>
-          <WorkItem
-            style={todoListBgImg}
-            title={"Todo List"}
-            pagesLink={'https://aivan3d66.github.io/toDoList-TS/'}
-            githubLink={'https://github.com/aivan3d66/toDoList-TS'}
-            description={"Simple todo list with storage of data on remote server. Technology stack: React, Redux (Redux-thunk), REST API, Storybook, Unit and snapshot tests"}
-          />
-          <WorkItem
-            style={cardImg}
-            title={"Cards"}
-            pagesLink={'https://ershisan99.github.io/cards/'}
-            githubLink={'https://github.com/aivan3d66/cards'}
-            description={"Team IT-INCUBATOR project. We used next technologies: JS, TypeScript, React, Redux, Redux-toolkit, Tailwind UI"}
-          />
-
-          <WorkItem
-            style={shopImg}
-            title={"Food-shop"}
-            pagesLink={'https://aivan3d66.github.io/food-shop/'}
-            githubLink={'https://github.com/aivan3d66/food-shop'}
-            description={"Test project with strong stack. Front-end: TypeScript, React, Redux, Redux-toolkit, Apollo GraphQL, Styled Components. Back-end (deployed to Heroku): Node.js + Express, MongoDB, GraphQL"}
-          />
-
-          <WorkItem
-            style={bigTripBgImg}
-            title={"Big trip"}
-            pagesLink={'https://aivan3d66.github.io/Big-trip/'}
-            githubLink={'https://github.com/aivan3d66/Big-trip'}
-            description={"Features: The MVP architectural pattern is used, based on the Object-oriented programming (classes), Off-line mode (Service Worker)"}
-          />
-          <WorkItem
-            style={socialNetworkBgImg}
-            title={"Social Network"}
-            pagesLink={'https://aivan3d66.github.io/socialNetwork-project/'}
-            githubLink={'https://github.com/aivan3d66/socialNetwork-project'}
-            description={"Mini-version of Social network. It is possible to write messages to another users, add posts, and search for new friends."}
-          />
-          <WorkItem
-            style={maroonBgImg}
-            title={"Maroon"}
-            pagesLink={'https://aivan3d66.github.io/Maroon/'}
-            githubLink={'https://github.com/aivan3d66/Maroon'}
-            description={"E-shop website with main, catalog, product card pages. Adaptive design (desktop, tablet, mobile) flex and grid-based layout, \"Mobile first\""}
-          />
-          <WorkItem
-            style={converterImg}
-            title={"Currency exchange"}
-            pagesLink={'https://currency-exchange-converter.herokuapp.com/'}
-            githubLink={'https://github.com/aivan3d66/currency-converter'}
-            description={"Simple Currency exchange app. Using stack: React + MobX library of state management, Styled components and Heroku deploying"}
-          />
-          <WorkItem
-            style={movieImg}
-            title={"Movie-graph"}
-            pagesLink={'https://movie-graph-front.herokuapp.com/'}
-            githubLink={'https://github.com/aivan3d66/movie-graph'}
-            description={"Movie-manager APP. For Back-end I used: Node.js + Express, GraphQl, MongoDB and Front-end: React, Apollo GraphQL, Material UI. All this stuff deploying to Heroku"}
-          />
-          <WorkItem
-            style={gameImg}
-            title={"2048"}
-            pagesLink={'https://the-2048-game-vanilla-js.herokuapp.com/'}
-            githubLink={'https://github.com/aivan3d66/game-2048'}
-            description={"A copy of a famous mobile game created with React.js and Heroku deploying"}
-          />
+          {
+            WorksData.map((m, i: number) => {
+              return (
+                <WorkItem
+                  key={i}
+                  title={m.title}
+                  description={m.description}
+                  pagesLink={m.pagesLink}
+                  githubLink={m.githubLink}
+                  style={m.bgImage}
+                />
+              )
+            })
+          }
         </div>
       </div>
     </section>
